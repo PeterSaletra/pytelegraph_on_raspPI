bb2aef777dfb7bf83f21c2cbd7bf9a852a77997b
PeterSaletra <peter.saletra@gamil.com>
2024-03-27 23:15:33 +0000

    Small correction of user experience

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dot
 button = Button(5)
 led = LED(6)
 
+word = ''
 code =  ''
 start =  0
 end = 0
 gstart = 0
 
+
 def starttime():
     global start
     start =  time.time()
     led.on()
+
 
 def endtime():
     global end
@@ -28,8 +31,16 @@ def endtime():
         code += '-'
     elif end - start < 0.5:
         code += '.'
-    print(code)
+    print(f"Actuall code: {code}")
     gstart = time.time()
+
+
+def blink():
+    for i in range(12):
+        led.toggle()
+        time.sleep(0.05)
+    led.off()
+
 
 def setup():
     lcd.clear()
@@ -47,6 +58,7 @@ def setup():
 def run():
     global gstart
     global code
+    global word
     posx = 0
     posy = 0
     node = makeMorseTree()
@@ -54,21 +66,38 @@ def run():
         if not button.is_pressed:
             gend = time.time()
             if gend - gstart > 2 and code:
+                led.on()
                 letter = findLetter(node, code, 0)
-                print(letter)
                 if letter:
-                    code = ''
                     lcd.cursor_pos = (posy, posx)
                     lcd.write_string(letter)
+                    word += letter
+                    print(f"Actuall code: {letter}")
                     posx += 1
                     if posx == 16:
                         posx = 0
                         posy += 1
                     if posy == 2:
                         posy = 0
+                else:
+                    lcd.clear()
+                    lcd.cursor_pos = (0, 0)
+                    lcd.write_string("Wrong morse code")
+                    time.sleep(2)
+                    lcd.clear()
+                    lcd.write_string(word)
+                code = ''
                 gstart = gend
-            elif gend - gstart >= 7:
+                time.sleep(0.5)
+                led.off()
+            elif gend - gstart >= 7 and word:
                 lcd.clear()
+                blink()
+                code = ''
+                word = ''
+                posx = 0
+                posy = 0
+                led.off()
         
         button.when_pressed = starttime
         button.when_released = endtime
