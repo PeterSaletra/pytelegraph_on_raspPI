53c46b9c84886171da89007cf377ee566e110837
PeterSaletra <peter.saletra@gmail.com>
2024-03-28 18:58:31 +0100

    Added HTTP Serwer

---
+++ server/server.py
@@ -0,0 +1,30 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-length'])
+        post_data = self.rfile.read(content_length)
+        parsed_data = urllib.parse.parse_qs(post_data.decode())
+        message = parsed_data.get('word', [''])[0]
+
+        with open('messages.txt', '+a') as file:
+            file.write(message + "\n")
+
+        self.send_response(201)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Wiadomosc poprawieni zapiasana")
+
+    def do_GET(self):
+        self.send_response(200)
+    
+
+def main():
+    server_address = ('192.170.1.133', 8000)
+    httpd = HTTPServer(server_address, HTTPRequestHandler)
+    print("Server working correctly")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
