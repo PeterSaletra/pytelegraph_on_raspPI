7cbf4c5ca4661233ff539da4e6d6d63861f60264
PeterSaletra <peter.saletra@gamil.com>
2024-03-28 17:57:40 +0000

    Added httpClient

--- libs/httpClient.py
+++ libs/httpClient.py
@@ -8,7 +8,17 @@ class HTTPClient:
         self.url = "http://" + str(self.ip_address) + ":" + str(port)
 
     def checkConnection():
-        if HTTPStatus.OK request.get(url = self.url):
+        r =  request.get(url = self.url)
+        if HTTPStatus.OK == r.status_code:
             return True
 
         return Fasle
+
+    def sendWord(word):
+        params = {"word": word}
+        r = request.post(url=self.url, data=paramms)
+        if HTTPStatus.CREATED == r.status_code:
+            print("Wiadomość poprawieni dostarczona")
+            return True
+        print("Nie dostarczono wiadomości")
+        retrun False
